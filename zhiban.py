f03653cbaed891e7475dd7efb8463406ff0b6e1e
mahongyin <1976222027@qq.com>
2023-06-30 23:09:34 +0800

    Update zhiban.py
    ok

--- zhiban.py
+++ zhiban.py
@@ -1,21 +1,20 @@
 import datetime
 import json
-from datetime import datetime
 import requests
 
 jiaban = [
-    ["付海申", "李学林", "马洪印"],
-    ["李运琴", "田青林", "李晓欢"],
-    ["张志朋", "任诘嘉", "周福生"],
-    ["周广鹏", "纪元", "赵昱胜"],
-    ["卢超", "刘新顺"], [], []]
+    ["付海申"， "李学林"， "马洪印"]，
+    ["李运琴"， "田青林"， "李晓欢"]，
+    ["张志朋"， "任诘嘉"， "周福生"]，
+    ["周广鹏"， "纪元"， "赵昱胜"]，
+    ["卢超"， "刘新顺"]， []， []]
 
 
 # 计算两个日期相差天数，自定义函数名，和两个日期的变量名。
 # time.strptime()函数根据指定的格式把一个时间字符串解析为时间元组。
 def getDays(str1, str2):
-    date1 = datetime.strptime(str1[0:10], "%Y-%m-%d")
-    date2 = datetime.strptime(str2[0:10], "%Y-%m-%d")
+    date1 = datetime.datetime.strptime(str1[0:10], "%Y-%m-%d")
+    date2 = datetime.datetime.strptime(str2[0:10], "%Y-%m-%d")
     num = (date2 - date1).days
     return num
 
@@ -28,35 +27,38 @@ def doSort(time):
         if zhou > 4:
             zhou = zhou % 5
         if zhou != 0:
-            # if (index < zhou) {
-            # index = index + (5 - zhou);
-            # } else if (index == zhou) {
-            # index = 0;
-            # } else {
-            # index = index - zhou;
-            # }
+            # if index < zhou:
+            #     index = index + (5 - zhou)
+            # elif index == zhou:
+            #     index = 0
+            # else :
+            #     index = index - zhou
+
             if index == 5 - zhou:
                 index = 0
             elif index < 5 - zhou:
                 index = index + zhou
             else:
                 index = index - (5 - zhou)
-    print("今日值班" + time + '===>' + str(jiaban[index]) + "\n")
-    return str(jiaban[index])
+
+    return "今日值班" + time + '\n' + str(jiaban[index])
 
 
 def get_datas(my_time):
     name = doSort(my_time)
     # 返回钉钉模型数据，1:文本；
+    if '马洪印' in name:
+        mobie = '15732164757'
+    else:
+        mobie = ''
     my_data = {
         "msgtype": "text",
-        "title": "通知",
         "text": {
-            "content": "今日值班如下:" + "\n" + name + "\n"
+            "content": "通知:" + "\n" + name + "\n"
         },
         "at": {
             "atMobiles": [
-                " "
+                mobie
             ],
             "isAtAll": False  # 这个参数为true好像是@所有人的意思
         }
@@ -64,14 +66,16 @@ def get_datas(my_time):
     return my_data
 
 
+
 if __name__ == "__main__":
+    dd_token = 'ba35e19ca18333d8efd8ff8da2535ab1915a2457997472c8f60e3578b694e269'
     my_data = get_datas(datetime.today().strftime("%Y-%m-%d"))
     header = {
         "Content-Type": "application/json",
         "Charset": "UTF-8"
     }
 
-    my_url = "https://oapi.dingtalk.com/robot/send?access_token=ba35e19ca18333d8efd8ff8da2535ab1915a2457997472c8f60e3578b694e269"
+    my_url = "https://oapi.dingtalk.com/robot/send?access_token="+dd_token
     ret = requests.post(url=my_url, data=json.dumps(my_data), headers=header)
     if ret.status_code == 200:
-        print(json.dumps(my_data))
+        print(ret.text)
