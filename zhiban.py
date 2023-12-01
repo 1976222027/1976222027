24a95ef693fe3c563b26aa4aa2b75d6d8fa69f47
mahongyin <1976222027@qq.com>
2023-12-01 14:01:32 +0800

    更新规则 每天17:40

--- zhiban.py
+++ zhiban.py
@@ -4,26 +4,32 @@ import os
 import requests
 
 jiaban = [
+    ["周福生", "李晓欢"],
     ["付海申", "李学林", "马洪印"],
-    ["李运琴", "田青林", "李晓欢"],
-    ["张志朋", "任诘嘉", "周福生"],
-    ["周广鹏", "纪元", "赵昱胜"],
-    ["卢超", "刘新顺"], [], []]
+    ["李运琴", "田青林"],
+    ["张志朋", "任诘嘉"],
+    ["周广鹏", "赵昱胜"],
+    ["纪元", "刘新顺"]
+]
 
 
 # 计算两个日期相差天数，自定义函数名，和两个日期的变量名。
 # time.strptime()函数根据指定的格式把一个时间字符串解析为时间元组。
-def getDays(str1, str2):
+def getGapDays(str1, str2):
     date1 = datetime.datetime.strptime(str1[0:10], "%Y-%m-%d")
     date2 = datetime.datetime.strptime(str2[0:10], "%Y-%m-%d")
     num = (date2 - date1).days
     return num
 
 
+# 从2023-12-6开始，每周一三四为值班 按着顺序依次循环jiaban表
+
+# 周1/3/4
 def doSort(time):
-    countD = getDays('2023-7-3', time)
+    # 今天和初始天差几天
+    countD = getGapDays('2023-7-3', time)
     zhou = countD // 7  # //取整
-    index = countD % 7
+    index = countD % 7  # %取余
     if index < 5:
         if zhou > 4:
             zhou = zhou % 5
@@ -45,8 +51,10 @@ def doSort(time):
     return "今日值班" + time + '\n' + str(jiaban[index])
 
 
-def get_datas(my_time):
-    name = doSort(my_time)
+def get_datas(today):
+    # my_time = today.strftime('%Y-%m-%d')
+    # name = doSort(my_time)
+    name = getDateOfficer(today)
     # 返回钉钉模型数据，1:文本；
     if '马洪印' in name:
         mobie = os.environ['MOBIE']
@@ -67,7 +75,28 @@ def get_datas(my_time):
     return my_data
 
 
-if __name__ == "__main__":
+# 123 456 123 456
+def getDateOfficer(current_date):
+    week = [0, 2, 3]
+    current_weekday = current_date.weekday()
+    # 检查是否为周一、周三或周四
+    if current_weekday in week:  # 周一是0，周三是2，周四是3
+        day_of_week = current_date.strftime("%A")
+        # 只用于比较 不用关心时区
+        start_date = datetime.date(2023, 12, 4)
+        currentdate = datetime.date.today()
+        delta = currentdate - start_date
+        week_num = delta.days // 7
+        index = week.index(current_date.weekday())
+        # 0头周 1尾周
+        if week_num % 2 != 0:
+            index += 3
+        day_officer = jiaban[index]
+        return f"今天是{day_of_week}，以下是今天的值班人员：{day_officer}"
+    else:
+        return "今天不是周一、周三或周四，不需要安排值班。"
+
+if __name__ == "__main__2":
     # 时区问题
     # SHA_TZ = datetime.timezone(
     #     datetime.timedelta(hours=8),
@@ -78,11 +107,10 @@ if __name__ == "__main__":
     # beijing_now = utc_now.astimezone(SHA_TZ)
     # today = beijing_now.date().strftime("%Y-%m-%d")
 
-    now = datetime.datetime.utcnow()  # utc时间
+    now = datetime.datetime.utcnow()  # utc时间 因为服务器在国外的将不是北京时间
     bj_time = now + datetime.timedelta(hours=8)  # 直接加8，很傻瓜
-    today = bj_time.strftime('%Y-%m-%d')
-    # today =datetime.datetime.today().strftime("%Y-%m-%d")
-    my_data = get_datas(today)
+    # today =datetime.datetime.today().strftime("%Y-%m-%d") #时区问题
+    my_data = get_datas(bj_time)
     header = {
         "Content-Type": "application/json",
         "Charset": "UTF-8"
