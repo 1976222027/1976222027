9c5b8c63fda04b7fb85d45452f9817f6be203418
mahongyin <1976222027@qq.com>
2025-03-12 11:35:27 +0800

    0

--- xjsign.py
+++ xjsign.py
@@ -79,7 +79,8 @@ def calculate_signature(key, data):
 if __name__ == '__main__':
     host = 'iios.bxguwen.com'
     secret = '63add3a71874e8200be66b14c826ae96'
-    token = os.environ["XJ_TOKEN"]#'6131376237306365626263663432616362303863376464376565386366653965'
+    token = os.environ['XJ_TOKEN']
+    #'6131376237306365626263663432616362303863376464376565386366653965'
     # 接口好像没有验证 secret, 只是定期更新token
     header = {
         'Host': host,
